ee5b990aa2eb8d48a7c30afb0a67a705229c9b06
roomjs <zzzz1zzzz1@naver.com>
2022-10-21 01:59:41 +0900

    blog add

---
+++ my-hoodie-blog/blog-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: "Weenu the Seenu",
+  description: "가보자고",
+  author: "Hudi",
+  siteUrl: "https://devhudi.github.io/gatsby-starter-hoodie/",
+  links: {
+    github: "https://github.com/devHudi",
+    linkedIn: "https://linkedin.com",
+    facebook: "https://www.facebook.com",
+    instagram: "https://www.instagram.com",
+    email: "mailto:devhudi@gmail.com",
+  },
+  utterances: {
+    repo: "devHudi/gatsby-starter-hoodie",
+    type: "pathname",
+  },
+}
